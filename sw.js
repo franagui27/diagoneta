2152f1e4e7d7c8efb5465e8c320184aa4d47c0e7
franagui27 <paloaguirre84@gmail.com>
2025-01-29 20:52:55 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener("install", function (event) {
-  console.log("Service Worker instalado");
+self.addEventListener('install', function(event) {
+    console.log('Service Worker instalado');
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+    event.respondWith(fetch(event.request));
 });
